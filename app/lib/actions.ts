b40459d98eccea18dba436bdc29316990ca43769
Kevin Panko <pankok@gmail.com>
2024-10-14 00:14:55 -0400

    Added error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,17 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    }
+    catch(error) {
+        return {
+            message: 'Database error: Failed to create invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -44,18 +51,32 @@ export async function updateInvoice(id: string, formData: FormData) {
     const amountInCents = amount * 100;
     // Leave out date because why not
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    }
+    catch(error) {
+        return {
+            message: 'Database error: Failed to update invoice.',
+        };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
 
-    revalidatePath('/dashboard/invoices');
+        revalidatePath('/dashboard/invoices');
+
+        return { message: 'Deleted invoice.' };
+    }
+    catch(error) {
+        return { message: 'Database error: Failed to delete invoice.' };
+    }
 }
