cdb3c1d1689756ee8f4720612aa4a456f3b6a10b
Kevin Panko <pankok@gmail.com>
2024-10-14 20:16:19 -0400

    added authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -116,5 +118,26 @@ export async function deleteInvoice(id: string) {
     }
     catch {
         return { message: 'Database error: Failed to delete invoice.' };
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    }
+    catch(error) {
+        if(error instanceof AuthError) {
+            switch(error.type) {
+                case 'CredentialsSignin':
+                    return 'Wrong credentials.';
+                default:
+                    return 'Something bad in the auth logic.';
+            }
+        }
+
+        throw error;
     }
 }
